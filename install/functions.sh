088b89c40710925a153c8223b8c4aea75ac504a2
Bruno Cornec <bruno@victoria.frmug.org>
2024-03-27 16:23:58 +0100

    Check WODAPIDBUSER and WODAPIDBPWD which could be null and reset to empty

--- install/functions.sh
+++ install/functions.sh
@@ -12,11 +12,23 @@ set -e
 get_wodapidb_userpwd() {
 if [ -f "$ANSIBLEDIR/group_vars/$PBKDIR" ]; then
 	WODAPIDBUSER=`cat "$ANSIBLEDIR/group_vars/$PBKDIR" | yq '.WODAPIDBUSER'`
+	if [ _"$WODAPIDBUSER" = _"null" ]; then
+		WODAPIDBUSER=""
+	fi
 	WODAPIDBPWD=`cat "$ANSIBLEDIR/group_vars/$PBKDIR" | yq '.WODAPIDBPWD'`
+	if [ _"$WODAPIDBPWD" = _"null" ]; then
+		WODAPIDBPWD=""
+	fi
 fi
 if [ -f "$ANSIBLEPRIVDIR/group_vars/$PBKDIR" ]; then
 	WODAPIDBUSER=`cat "$ANSIBLEPRIVDIR/group_vars/$PBKDIR" | yq '.WODAPIDBUSER'`
+	if [ _"$WODAPIDBUSER" = _"null" ]; then
+		WODAPIDBUSER=""
+	fi
 	WODAPIDBPWD=`cat "$ANSIBLEPRIVDIR/group_vars/$PBKDIR" | yq '.WODAPIDBPWD'`
+	if [ _"$WODAPIDBPWD" = _"null" ]; then
+		WODAPIDBPWD=""
+	fi
 fi
 if [ _"$WODAPIDBUSER" = _"" ]; then
 	echo "You need to configure WODAPIDBUSER in your $PBKDIR ansible variable file"
